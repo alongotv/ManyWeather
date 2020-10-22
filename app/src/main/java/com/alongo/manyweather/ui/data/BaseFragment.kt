171b3d5b3685be7a593f6bb4419e447db7a69b1d
alongotv <pdastore1@gmail.com>
2020-10-23 02:00:55 +0300

    Add open class BaseFragment

---
+++ app/src/main/java/com/alongo/manyweather/ui/data/BaseFragment.kt
@@ -0,0 +1,21 @@
+package com.alongo.manyweather.ui.data
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dagger.android.support.DaggerFragment
+
+open class BaseFragment: DaggerFragment() {
+    lateinit var inflaterContext: Context
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        inflaterContext = inflater.context
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
