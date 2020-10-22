6b9295a1fd0cdb63f2b200cd192df2b4d3d0acd0
alongotv <pdastore1@gmail.com>
2020-10-23 02:02:41 +0300

    Add abstract functions contributeMainFragment, contributeGeolocationWeatherFragment to FragmentBuilderModule

--- app/src/main/java/com/alongo/manyweather/di/FragmentBuilderModule.kt
+++ app/src/main/java/com/alongo/manyweather/di/FragmentBuilderModule.kt
@@ -1,9 +1,16 @@
 package com.alongo.manyweather.di
 
+import com.alongo.manyweather.ui.fragment.GeolocationWeatherFragment
+import com.alongo.manyweather.ui.fragment.MainFragment
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
 
 
 @Module
 abstract class FragmentBuilderModule {
+    @ContributesAndroidInjector
+    abstract fun contributeMainFragment(): MainFragment
 
+    @ContributesAndroidInjector
+    abstract fun contributeGeolocationWeatherFragment(): GeolocationWeatherFragment
 }
