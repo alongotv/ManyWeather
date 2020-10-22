57f0cc0dd9e6150b26a1ad1135582ab1d2ae0409
alongotv <pdastore1@gmail.com>
2020-10-23 02:02:11 +0300

    Add abstract class ViewModelModule

---
+++ app/src/main/java/com/alongo/manyweather/di/ViewModelModule.kt
@@ -0,0 +1,27 @@
+package com.alongo.manyweather.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.alongo.manyweather.ui.fragment.GeolocationWeatherViewModel
+import com.alongo.manyweather.ui.fragment.MainViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+
+@Module
+abstract class ViewModelModule {
+
+    @Binds
+    internal abstract fun bindViewModelFactory(viewModelFactory: ViewModelFactory): ViewModelProvider.Factory
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainViewModel::class)
+    abstract fun bindMainViewModel(mainViewModel: MainViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(GeolocationWeatherViewModel::class)
+    abstract fun bindGeolocationWeatherViewModel(geolocationWeatherViewModel: GeolocationWeatherViewModel): ViewModel
+}
