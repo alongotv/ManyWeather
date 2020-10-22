1719e310ef25056d88c2809da2994de688188955
alongotv <pdastore1@gmail.com>
2020-10-23 02:00:38 +0300

    Add function getWeatherByCoordinates; Update getWeatherByCityName function to contain appid, units params

--- app/src/main/java/com/alongo/manyweather/remote/WeatherAPI.kt
+++ app/src/main/java/com/alongo/manyweather/remote/WeatherAPI.kt
@@ -1,5 +1,6 @@
 package com.alongo.manyweather.remote
 
+import com.alongo.manyweather.data.Weather
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -9,5 +10,18 @@ interface WeatherAPI {
 
     @Headers("Content-Type: application/json")
     @GET("weather")
-    fun getWeatherByCityName(@Query("q") cityName: String): Observable<String>
+    fun getWeatherByCityName(
+        @Query("q") cityName: String,
+        @Query("appid") openWeatherToken: String,
+        @Query("units") meausurementUnit: String = "metric"
+    ): Observable<Weather>
+
+    @Headers("Content-Type: application/json")
+    @GET("weather")
+    fun getWeatherByCoordinates(
+        @Query("lat") latitude: Double,
+        @Query("lon") longitude: Double,
+        @Query("appid") openWeatherToken: String,
+        @Query("units") meausurementUnit: String = "metric"
+    ): Observable<Weather>
 }
