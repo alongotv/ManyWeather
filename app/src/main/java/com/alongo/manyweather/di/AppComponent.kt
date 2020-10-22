3e462eecf96d3eb112651703c8ec5a4dfdf13006
alongotv <pdastore1@gmail.com>
2020-10-23 01:59:58 +0300

    Add DI Interface AppComponent

---
+++ app/src/main/java/com/alongo/manyweather/di/AppComponent.kt
@@ -0,0 +1,27 @@
+package com.alongo.manyweather.di
+
+import com.alongo.manyweather.AppDelegate
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [AppModule::class,
+        AndroidSupportInjectionModule::class,
+        ActivityBuilderModule::class,
+        ViewModelModule::class]
+)
+interface AppComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: AppDelegate): Builder
+
+        fun build(): AppComponent
+    }
+
+    fun inject(app: AppDelegate)
+}
