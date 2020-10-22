f2d087d4f1b616d2a195d4699024cef6835aa223
alongotv <pdastore1@gmail.com>
2020-10-23 02:01:47 +0300

    Add class ViewModelFactory

---
+++ app/src/main/java/com/alongo/manyweather/di/ViewModelFactory.kt
@@ -0,0 +1,31 @@
+package com.alongo.manyweather.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+
+@Singleton
+class ViewModelFactory @Inject constructor(private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>) :
+    ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        var creator: Provider<out ViewModel>? = creators[modelClass]
+
+        if (creator == null) {
+            for ((key, value) in creators) {
+                if (modelClass.isAssignableFrom(key)) {
+                    creator = value
+                    break
+                }
+            }
+        }
+        requireNotNull(creator) { "Unknown ViewModel class: $modelClass" }
+        try {
+            return creator.get() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+}
