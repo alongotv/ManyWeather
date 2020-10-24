0e7a23c33be92867b31fe04c0a267167fa6d4468
Vladimir <pdastore1@gmail.com>
2020-10-24 20:44:34 +0300

    Add const WEATHER_API_ICON_STORAGE_ENDPOINT

--- app/src/main/java/com/alongo/manyweather/utilities/Constants.kt
+++ app/src/main/java/com/alongo/manyweather/utilities/Constants.kt
@@ -1,4 +1,5 @@
 package com.alongo.manyweather.utilities
 
 const val WEATHER_API_ENDPOINT = "https://api.openweathermap.org/data/2.5/"
+const val WEATHER_API_ICON_STORAGE_ENDPOINT = "https://openweathermap.org/img/wn/"
 const val PERMISSION_GEOLOCATION_CODE = 123
