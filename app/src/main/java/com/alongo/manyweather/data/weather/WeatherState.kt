631aa535d297523d27244b9187587d1647d9ca86
Vladimir <pdastore1@gmail.com>
2020-10-24 23:24:36 +0300

    Add data class WeatherState

---
+++ app/src/main/java/com/alongo/manyweather/data/weather/WeatherState.kt
@@ -0,0 +1,12 @@
+package com.alongo.manyweather.data.weather
+
+import com.google.gson.annotations.SerializedName
+
+data class WeatherState (
+    val id: Int,
+    @SerializedName("main")
+    val title: String,
+    val description: String,
+    @SerializedName("icon")
+    val iconName: String //	Should be used to retrieve the image from the backend. Template: https://openweathermap.org/img/wn/{iconName}.png
+)
