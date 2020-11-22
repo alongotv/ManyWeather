1f0f3c80f1f1c4c9c00bc2170d7f84f47b251acc
vlavet <pdastore1@gmail.com>
2020-11-23 02:58:44 +0300

    Fix Flow.doOnFirst not emitting values after the first one

--- app/src/main/java/com/alongo/manyweather/utilities/ReactiveExtensions.kt
+++ app/src/main/java/com/alongo/manyweather/utilities/ReactiveExtensions.kt
@@ -3,4 +3,4 @@ import kotlinx.coroutines.flow.*
 
 fun <T> Observable<T>.doBeforeFirst(onFirstAction: (T) -> Unit): Observable<T> = take(1).doOnNext { onFirstAction.invoke(it) }.concatWith(skip(1))
 
-fun <T> Flow<T>.doOnFirst(onFirstAction: (T) -> Unit): Flow<T> = take(1).onEach { onFirstAction.invoke(it) }.onCompletion{ drop(1)}
+fun <T> Flow<T>.doBeforeFirst(onFirstAction: (T) -> Unit): Flow<T> = take(1).onEach { onFirstAction.invoke(it) }.flatMapConcat { this }
