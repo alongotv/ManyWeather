2bdd0101a04f0fbf10836097903474aca8b38a41
vlavet <pdastore1@gmail.com>
2020-11-22 03:46:53 +0300

    Replace weatherSubject, errorSubject subjects with LiveData properties

--- app/src/main/java/com/alongo/manyweather/ui/fragment/MainViewModel.kt
+++ app/src/main/java/com/alongo/manyweather/ui/fragment/MainViewModel.kt
@@ -1,17 +1,22 @@
 package com.alongo.manyweather.ui.fragment
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.alongo.manyweather.data.weather.Weather
 import com.alongo.manyweather.remote.WeatherAPI
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import io.reactivex.subjects.BehaviorSubject
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(private val weatherAPI: WeatherAPI) : ViewModel() {
-    val weatherSubject: BehaviorSubject<Weather> = BehaviorSubject.create()
-    val errorSubject: BehaviorSubject<String> = BehaviorSubject.create()
+    val weatherData: MutableLiveData<Weather> by lazy {
+        MutableLiveData<Weather>()
+    }
+
+    val errorData:  MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
 
     private val compositeDisposable = CompositeDisposable()
 
@@ -22,9 +27,9 @@ class MainViewModel @Inject constructor(private val weatherAPI: WeatherAPI) : Vi
             ).subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({ weather: Weather ->
-                weatherSubject.onNext(weather)
+                weatherData.postValue(weather)
             }, { error ->
-                errorSubject.onNext("${error.javaClass}: ${error.message ?: "Empty error"}")
+                errorData.postValue("${error.javaClass}: ${error.message ?: "Empty error"}")
             })
         compositeDisposable.add(weatherSubscription)
     }
