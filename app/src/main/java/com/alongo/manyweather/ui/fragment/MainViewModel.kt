5d78a325bb6118f160ed578266a010629b124858
vlavet <pdastore1@gmail.com>
2020-11-23 03:02:26 +0300

    Encapsulate weatherData and errorData inside MainViewModel

--- app/src/main/java/com/alongo/manyweather/ui/fragment/MainViewModel.kt
+++ app/src/main/java/com/alongo/manyweather/ui/fragment/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.alongo.manyweather.ui.fragment
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.alongo.manyweather.data.weather.Weather
@@ -10,26 +11,29 @@ import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(private val weatherAPI: WeatherAPI) : ViewModel() {
-    val weatherData: MutableLiveData<Weather> by lazy {
+    private val _weatherData: MutableLiveData<Weather> by lazy {
         MutableLiveData<Weather>()
     }
+    val weatherData: LiveData<Weather>
+        get() = _weatherData
 
-    val errorData:  MutableLiveData<String> by lazy {
-        MutableLiveData<String>()
-    }
+    private val _errorData: MutableLiveData<String> by lazy { MutableLiveData<String>() }
+    val errorData: LiveData<String>
+        get() = _errorData
 
     private val compositeDisposable = CompositeDisposable()
 
     fun getWeatherByCityName(name: String) {
+        errorData
         val weatherSubscription = weatherAPI.getWeatherByCityName(
-                cityName = name,
-                openWeatherToken = "1ce0002adc6d659ab342e693b333c059"
-            ).subscribeOn(Schedulers.io())
+            cityName = name,
+            openWeatherToken = "1ce0002adc6d659ab342e693b333c059"
+        ).subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({ weather: Weather ->
-                weatherData.postValue(weather)
+                _weatherData.postValue(weather)
             }, { error ->
-                errorData.postValue("${error.javaClass}: ${error.message ?: "Empty error"}")
+                _errorData.postValue("${error.javaClass}: ${error.message ?: "Empty error"}")
             })
         compositeDisposable.add(weatherSubscription)
     }
